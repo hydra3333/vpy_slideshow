61e0b611bb535a4a05803867e0b0319ba32f7bd0
hydra3333 <10749357+hydra3333@users.noreply.github.com>
2023-03-11 09:58:43 +1030

    Add files via upload

--- _AI_05.bat
+++ _AI_05.bat
@@ -1,12 +1,37 @@
-@ECHO ON
+@ECHO OFF
 @setlocal ENABLEDELAYEDEXPANSION
 @setlocal enableextensions
 
 REM produces a video of a folder
 
-set   "script.\_AI_05.vpy"
-set "ini_file=.\_AI_05.ini"
-set "mp4_file=.\_AI_05.mp4"
+set      "pth=G:\DVD\PAT-SLIDESHOWS\_AI_05_in_development"
+set   "script=%pth%\_AI_05.vpy"
+set "ini_file=.\SLIDESHOW_PARAMETERS.ini"
+set "mp4_file=%pth%\_AI_05.mp4"
+
+set "cmd1="
+set "cmd1=%cmd1%"C:\SOFTWARE\Vapoursynth-x64\VSPipe.exe" --container y4m "%script%" - "
+REM set "cmd1=%cmd1%"C:\SOFTWARE\Vapoursynth-x64\VSPipe.exe" --progress --filter-time --container y4m "%script%" - "
+
+set "cmd2="
+set "cmd2=%cmd2%"C:\SOFTWARE\Vapoursynth-x64\ffmpeg_OpenCL.exe" -hide_banner -v verbose -stats "
+set "cmd2=%cmd2%-f yuv4mpegpipe -i pipe: "
+set "cmd2=%cmd2%-probesize 200M "
+set "cmd2=%cmd2%-analyzeduration 200M "
+set "cmd2=%cmd2%-sws_flags lanczos+accurate_rnd+full_chroma_int+full_chroma_inp "
+set "cmd2=%cmd2%-filter_complex 'colorspace=all=bt709:space=bt709:trc=bt709:primaries=bt709:range=pc:format=yuv420p:fast=0,format=yuv420p,setdar=16/9' "
+set "cmd2=%cmd2%-colorspace bt709 -color_primaries bt709 -color_trc bt709 -color_range pc -strict experimental "
+set "cmd2=%cmd2%-c:v h264_nvenc -preset p7 -multipass fullres -forced-idr 1 -g 25 -coder:v cabac -spatial-aq 1 -temporal-aq 1 "
+set "cmd2=%cmd2%-dpb_size 0 -bf:v 3 -b_ref_mode:v 0 -rc:v vbr "
+set "cmd2=%cmd2%-b:v 9000000 -minrate:v 3000000 -maxrate:v 15000000 -bufsize 15000000 "
+set "cmd2=%cmd2%-profile:v high -level 5.2 -movflags +faststart+write_colr "
+set "cmd2=%cmd2%-an "
+set "cmd2=%cmd2%-y "%mp4_file%" "
+
+@echo on
+
+pushd C:\SOFTWARE\Vapoursynth-x64
+cd
 
 DEL "%ini_file%">NUL 2>&1
 echo [slideshow]>>"%ini_file%"
@@ -15,9 +40,25 @@ echo temp_directory = D:\TEMP>>"%ini_file%"
 echo recursive = True>>"%ini_file%"
 type "%ini_file%"
 
-"C:\SOFTWARE\Vapoursynth-x64\VSPipe.exe --progress --filter-time --container y4m "%script"% - | ^
+.\python.exe "%script%"
+
+popd
+
+pause
+exit
+
+pushd C:\SOFTWARE\Vapoursynth-x64
+cd
+
+DEL "%ini_file%">NUL 2>&1
+echo [slideshow]>>"%ini_file%"
+echo directory = G:\DVD\PAT-SLIDESHOWS\_AI_05_in_development\0TEST>>"%ini_file%"
+echo temp_directory = D:\TEMP>>"%ini_file%"
+echo recursive = True>>"%ini_file%"
+type "%ini_file%"
+
 "C:\SOFTWARE\Vapoursynth-x64\ffmpeg_OpenCL.exe" -hide_banner -v verbose -stats ^
--f yuv4mpegpipe -i pipe: ^
+-f vapoursynth -i "%script%" ^
 -probesize 200M ^
 -analyzeduration 200M ^
 -sws_flags lanczos+accurate_rnd+full_chroma_int+full_chroma_inp ^
@@ -29,6 +70,29 @@ type "%ini_file%"
 -profile:v high -level 5.2 -movflags +faststart+write_colr ^
 -an ^
 -y "%mp4_file%"
+
+popd
+
+@echo off
+
+pause
+exit
+
+echo %cmd1% ^| %cmd2%
+
+pushd C:\SOFTWARE\Vapoursynth-x64
+cd
+
+DEL "%ini_file%">NUL 2>&1
+echo [slideshow]>>"%ini_file%"
+echo directory = G:\DVD\PAT-SLIDESHOWS\_AI_05_in_development\0TEST>>"%ini_file%"
+echo temp_directory = D:\TEMP>>"%ini_file%"
+echo recursive = True>>"%ini_file%"
+type "%ini_file%"
+
+%cmd1% | %cmd2%
+
+popd
 
 pause
 exit
@@ -50,3 +114,10 @@ exit
 -an ^
 -y "%mp4_file%"
 
+
+
+
+"C:\SOFTWARE\Vapoursynth-x64\VSPipe.exe" --container y4m "G:\DVD\PAT-SLIDESHOWS\_AI_05_in_development\_AI_05.vpy" - >NUL
+"C:\SOFTWARE\Vapoursynth-x64\VSPipe.exe" --container y4m "G:\DVD\PAT-SLIDESHOWS\_AI_05_in_development\_AI_04.vpy" - >NUL
+
+"C:\SOFTWARE\Vapoursynth-x64\VSPipe.exe" --container y4m "%script%" -
---
+++ _AI_06.bat
@@ -0,0 +1,172 @@
+@ECHO OFF
+@setlocal ENABLEDELAYEDEXPANSION
+@setlocal enableextensions
+
+REM produces a video of a folder
+
+set "vs_path=C:\SOFTWARE\Vapoursynth-x64"
+if /I NOT "%vs_path:~-1%" == "\" (set "vs_path=%vs_path%\")
+
+REM set "pth=G:\DVD\PAT-SLIDESHOWS\_AI_06_in_development"
+REM if /I NOT "%pth:~-1%" == "\" (set "pth=%pth%\")
+
+set "pwd=%CD%"
+if /I NOT "%pwd:~-1%" == "\" (set "pwd=%pwd%\")
+
+set   "script=%pwd%\_AI_06.vpy"
+set "ini_file=%pwd%SLIDESHOW_PARAMETERS.ini"
+set "mp4_file=%pwd%\_AI_06.mp4"
+
+set bv=9000000
+set bv_min=3000000
+set bv_max=15000000
+set bv_buf=15000000
+
+call :using_python_only
+pause
+goto :eof
+
+call :using_vapoursynth_input
+pause
+goto :eof
+
+call :using_vspipe_input
+pause
+goto :eof
+
+
+call :convert_to_dvd_mpg
+pause
+goto :eof
+
+pause
+goto :eof
+
+
+
+:using_python_only
+@ECHO ON
+@setlocal ENABLEDELAYEDEXPANSION
+@setlocal enableextensions
+
+REM +++ just run python over it to see debug output
+REM pushd "%vs_path%"
+REM cd
+DEL "%ini_file%">NUL 2>&1
+echo [slideshow]>>"%ini_file%"
+echo directory = G:\DVD\PAT-SLIDESHOWS\_AI_05_in_development\1TEST>>"%ini_file%"
+echo temp_directory = D:\TEMP>>"%ini_file%"
+echo recursive = True>>"%ini_file%"
+type "%ini_file%"
+REM the script *should* have converted everythng to bt.709
+"%vs_path%python.exe" "%script%"
+REM popd
+goto :eof
+
+
+:using_vapoursynth_input
+@ECHO ON
+@setlocal ENABLEDELAYEDEXPANSION
+@setlocal enableextensions
+
+REM +++ try a raw run with vapoursynth input
+REM pushd "%vs_path%"
+REM cd
+DEL "%ini_file%">NUL 2>&1
+echo [slideshow]>>"%ini_file%"
+echo directory = G:\DVD\PAT-SLIDESHOWS\_AI_06_in_development\0TEST>>"%ini_file%"
+echo temp_directory = D:\TEMP>>"%ini_file%"
+echo recursive = True>>"%ini_file%"
+type "%ini_file%"
+REM nvenc parameters suitable for an nvidia 2060Super and will fail on a lesser card eg 1050Ti, eg bf:v 3 -spatial-aq 1 -temporal-aq 1
+REM the script *should* have converted everythng to bt.709
+"%vs_path%ffmpeg_OpenCL.exe" -hide_banner -v verbose -stats ^
+-f vapoursynth -i "%script%" ^
+-probesize 200M ^
+-analyzeduration 200M ^
+-sws_flags lanczos+accurate_rnd+full_chroma_int+full_chroma_inp ^
+-filter_complex "colorspace=all=bt709:space=bt709:trc=bt709:primaries=bt709:range=pc:format=yuv420p:fast=0,format=yuv420p,setdar=16/9" ^
+-colorspace bt709 -color_primaries bt709 -color_trc bt709 -color_range pc -strict experimental ^
+-c:v h264_nvenc -preset p7 -multipass fullres -forced-idr 1 -g 25 -coder:v cabac -spatial-aq 1 -temporal-aq 1 ^
+-dpb_size 0 -bf:v 3 -b_ref_mode:v 0 -rc:v vbr ^
+-b:v %bv% -minrate:v %bv_min% -maxrate:v %bv_max% -bufsize %bv_buf% ^
+-profile:v high -level 5.2 -movflags +faststart+write_colr ^
+-an ^
+-y "%mp4_file%"
+REM popd
+goto :eof
+
+
+:using_vspipe_input
+@ECHO ON
+@setlocal ENABLEDELAYEDEXPANSION
+@setlocal enableextensions
+REM +++ try a run using a CMD and vspipe
+REM pushd "%vs_path%"
+REM cd
+DEL "%ini_file%">NUL 2>&1
+echo [slideshow]>>"%ini_file%"
+echo directory = G:\DVD\PAT-SLIDESHOWS\_AI_06_in_development\0TEST>>"%ini_file%"
+echo temp_directory = D:\TEMP>>"%ini_file%"
+echo recursive = True>>"%ini_file%"
+type "%ini_file%"
+REM nvenc parameters suitable for an nvidia 2060Super and will fail on a lesser card eg 1050Ti, eg bf:v 3 -spatial-aq 1 -temporal-aq 1
+REM the script *should* have converted everythng to bt.709
+REM cannot put pipe symbol where I need it, so separate the commands and use the pipe symbol  later
+set "cmd1="
+set "cmd1=%cmd1%"%vs_path%VSPipe.exe" --container y4m "%script%" - "
+REM set "cmd1=%cmd1%"C:\SOFTWARE\Vapoursynth-x64\VSPipe.exe" --progress --filter-time --container y4m "%script%" - "
+set "cmd2="
+set "cmd2=%cmd2%"%vs_path%ffmpeg_OpenCL.exe" -hide_banner -v verbose -stats "
+set "cmd2=%cmd2%-f yuv4mpegpipe -i pipe: "
+set "cmd2=%cmd2%-probesize 200M "
+set "cmd2=%cmd2%-analyzeduration 200M "
+set "cmd2=%cmd2%-sws_flags lanczos+accurate_rnd+full_chroma_int+full_chroma_inp "
+set "cmd2=%cmd2%-filter_complex 'colorspace=all=bt709:space=bt709:trc=bt709:primaries=bt709:range=pc:format=yuv420p:fast=0,format=yuv420p,setdar=16/9' "
+set "cmd2=%cmd2%-colorspace bt709 -color_primaries bt709 -color_trc bt709 -color_range pc -strict experimental "
+set "cmd2=%cmd2%-c:v h264_nvenc -preset p7 -multipass fullres -forced-idr 1 -g 25 -coder:v cabac -spatial-aq 1 -temporal-aq 1 "
+set "cmd2=%cmd2%-dpb_size 0 -bf:v 3 -b_ref_mode:v 0 -rc:v vbr "
+set "cmd2=%cmd2%-b:v %bv% -minrate:v %bv_min% -maxrate:v %bv_max% -bufsize %bv_buf% "
+set "cmd2=%cmd2%-profile:v high -level 5.2 -movflags +faststart+write_colr "
+set "cmd2=%cmd2%-an "
+set "cmd2=%cmd2%-y "%mp4_file%" "
+
+echo %cmd1% ^| %cmd2%
+%cmd1% | %cmd2%
+REM popd
+goto :eof
+
+
+
+pause
+goto :eof
+
+
+
+
+REM CONVERT TO DVD
+
+# SD DVD is -colorspace bt470bg -color_primaries bt470bg -color_trc gamma28 -color_range tv
+# 3900x with nvidia 2060 Super uses -spatial-aq 1 -temporal-aq 1 -dpb_size 0 -bf:v 3 -b_ref_mode:v 0 # otherwise omit these 5 parameters
+
+"C:\SOFTWARE\Vapoursynth-x64\ffmpeg_OpenCL.exe" -hide_banner -v verbose -stats ^
+-f vapoursynth -i "!script!" ^
+-probesize 200M ^
+-analyzeduration 200M ^
+-sws_flags lanczos+accurate_rnd+full_chroma_int+full_chroma_inp ^
+-filter_complex "colorspace=all=bt709:space=bt709:trc=bt709:primaries=bt709:range=pc:format=yuv420p:fast=0,format=yuv420p,setdar=16/9" ^
+-colorspace bt709 -color_primaries bt709 -color_trc bt709 -color_range pc -strict experimental ^
+-c:v h264_nvenc -preset p7 -multipass fullres -forced-idr 1 -g 25 -coder:v cabac -spatial-aq 1 -temporal-aq 1 ^
+-dpb_size 0 -bf:v 3 -b_ref_mode:v 0 -rc:v vbr ^
+-b:v 9000000 -minrate:v 3000000 -maxrate:v 15000000 -bufsize 15000000 ^
+-profile:v high -level 5.2 -movflags +faststart+write_colr ^
+-an ^
+-y "%mp4_file%"
+
+
+
+
+"C:\SOFTWARE\Vapoursynth-x64\VSPipe.exe" --container y4m "G:\DVD\PAT-SLIDESHOWS\_AI_06_in_development\_AI_06.vpy" - >NUL
+"C:\SOFTWARE\Vapoursynth-x64\VSPipe.exe" --container y4m "G:\DVD\PAT-SLIDESHOWS\_AI_06_in_development\_AI_04.vpy" - >NUL
+
+"C:\SOFTWARE\Vapoursynth-x64\VSPipe.exe" --container y4m "%script%" -
